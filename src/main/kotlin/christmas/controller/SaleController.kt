2e64b495ead69d88036ba48401ebbb276b848ead
stopstone <wltjr6432@naver.com>
2023-11-11 23:33:17 +0900

    feat: 평일세일 기능 추가

--- src/main/kotlin/christmas/controller/SaleController.kt
+++ src/main/kotlin/christmas/controller/SaleController.kt
@@ -12,14 +12,19 @@ class SaleController {
         val date = Date()
         val day = date.calculateDay(dateNumber)
 
-        println(day)
-        if(day == 5 || day == 6) {
-            saleOfWeekend(menuItems)
+        when(day) {
+            0,1,2,3,4 -> saleOfWeekday(menuItems)
+            5, 6 -> saleOfWeekend(menuItems)
         }
     }
 
     private fun saleOfWeekend(menuItems: List<MenuItem>) {
         val mainSale = menuItems.filter { it.menu.itemCategory == "메인" }
+        return mainSale.forEach { it.count * 2023 }
+    }
+
+    private fun saleOfWeekday(menuItems: List<MenuItem>) {
+        val mainSale = menuItems.filter { it.menu.itemCategory == "디저트" }
         return mainSale.forEach { it.count * 2023 }
     }
 }

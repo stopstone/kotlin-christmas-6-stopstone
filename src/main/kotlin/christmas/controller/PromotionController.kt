83ee29045d4afc6726cbb2cd3e094d6f8016af6b
stopstone <wltjr6432@naver.com>
2023-11-11 16:05:53 +0900

    style: 테스트하기 위해 internal 사용

--- src/main/kotlin/christmas/controller/PromotionController.kt
+++ src/main/kotlin/christmas/controller/PromotionController.kt
@@ -29,7 +29,7 @@ class PromotionController(private val inputView: InputView, private val outputVi
         }
     }
 
-    private fun validate(input: String?): Int {
+    internal fun validate(input: String?): Int {
         require(!input.isNullOrEmpty() && input.all { it.isDigit() }) {"${Messages.ERROR_MESSAGE} ${Messages.INVALID_MESSAGE}"}
         require(input.toInt() in 1..31) {"${Messages.ERROR_MESSAGE} ${Messages.INVALID_MESSAGE}"}
         return input.toInt()

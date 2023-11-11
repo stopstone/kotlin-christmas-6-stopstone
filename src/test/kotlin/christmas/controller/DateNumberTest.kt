cca680dc57030a3a9ef24dd91cf40ab015977803
stopstone <wltjr6432@naver.com>
2023-11-11 16:12:21 +0900

    test: Validator Object 분리로 인한 수정

--- src/test/kotlin/christmas/controller/DateNumberTest.kt
+++ src/test/kotlin/christmas/controller/DateNumberTest.kt
@@ -2,6 +2,7 @@ package christmas.controller
 
 import christmas.utils.Messages.ERROR_MESSAGE
 import christmas.utils.Messages.INVALID_MESSAGE
+import christmas.validator.ValidateDate.validate
 import christmas.view.InputView
 import christmas.view.OutputView
 import org.junit.jupiter.api.Assertions.*
@@ -11,8 +12,7 @@ class DateNumberTest {
     @Test
     fun `날짜 입력값에 아무 값이 없을 경우`() {
         try {
-            val promotionController = PromotionController(InputView(), OutputView())
-             promotionController.validate("")
+             validate("")
         } catch (e: IllegalArgumentException) {
             println(e.message)
             assertEquals("$ERROR_MESSAGE $INVALID_MESSAGE", e.message)
@@ -22,8 +22,7 @@ class DateNumberTest {
     @Test
     fun `날짜 입력값에 1에서 31이 아닌 다른 숫자가 들어왔을 경우`() {
         try {
-            val promotionController = PromotionController(InputView(), OutputView())
-            promotionController.validate("42")
+            validate("42")
         } catch (e: IllegalArgumentException) {
             println(e.message)
             assertEquals("$ERROR_MESSAGE $INVALID_MESSAGE", e.message)
@@ -33,8 +32,7 @@ class DateNumberTest {
     @Test
     fun `날짜 입력값에 숫자가 아닌 문자가 들어왔을 경우`() {
         try {
-            val promotionController = PromotionController(InputView(), OutputView())
-            promotionController.validate("우아한")
+            validate("우아한")
         } catch (e: IllegalArgumentException) {
             println(e.message)
             assertEquals("$ERROR_MESSAGE $INVALID_MESSAGE", e.message)

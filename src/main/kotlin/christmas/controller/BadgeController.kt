87d13f525a5037e7018f7612a63a03eac8651d02
stopstone <wltjr6432@naver.com>
2023-11-14 01:32:44 +0900

    move: 뱃지 관련 기능 분리

---
+++ src/main/kotlin/christmas/controller/BadgeController.kt
@@ -0,0 +1,17 @@
+package christmas.controller
+
+import christmas.model.Badge
+import christmas.view.OutputView
+
+class BadgeController(private val saleController: SaleController, private val outputView: OutputView) {
+    private fun choiceBadge(totalDiscount: Int): String {
+        val selectedBadge = Badge.entries.firstOrNull { totalDiscount >= it.threshold } ?: Badge.NONE
+        return selectedBadge.displayBadge
+    }
+
+    fun eventBadge() {
+        val totalDiscount = saleController.totalDiscountAmount()
+        val badgeKind = choiceBadge(totalDiscount)
+        outputView.printEventBadge(badgeKind)
+    }
+}

11ab9295453770fa3cc84609192caa12136b0b61
stopstone <wltjr6432@naver.com>
2023-11-15 23:53:38 +0900

    style: import 참조 변경

--- src/main/kotlin/christmas/model/Menu.kt
+++ src/main/kotlin/christmas/model/Menu.kt
@@ -1,21 +1,49 @@
 package christmas.model
 
 import christmas.utils.Constants
+import christmas.utils.Constants.APPETIZER
+import christmas.utils.Constants.BBQ
+import christmas.utils.Constants.BBQ_COST
+import christmas.utils.Constants.CAESAR_SALAD
+import christmas.utils.Constants.CAESAR_SALAD_COST
+import christmas.utils.Constants.CHAMPAGNE
+import christmas.utils.Constants.CHAMPAGNE_COST
+import christmas.utils.Constants.CHOCO_CAKE
+import christmas.utils.Constants.CHOCO_CAKE_COST
+import christmas.utils.Constants.CHRISTMAS_PASTA
+import christmas.utils.Constants.CHRISTMAS_PASTA_COST
+import christmas.utils.Constants.DESSERT
+import christmas.utils.Constants.DRINK
+import christmas.utils.Constants.ICE_CREAM
+import christmas.utils.Constants.ICE_CREAM_COST
+import christmas.utils.Constants.MAIN
+import christmas.utils.Constants.MUSHROOM_SOUP
+import christmas.utils.Constants.MUSHROOM_SOUP_COST
+import christmas.utils.Constants.RED_WINE
+import christmas.utils.Constants.RED_WINE_COST
+import christmas.utils.Constants.SEAFOOD_PASTA
+import christmas.utils.Constants.SEAFOOD_PASTA_COST
+import christmas.utils.Constants.TAPAS
+import christmas.utils.Constants.TAPAS_COST
+import christmas.utils.Constants.T_BORN
+import christmas.utils.Constants.T_BORN_COST
+import christmas.utils.Constants.ZERO_COKE
+import christmas.utils.Constants.ZERO_COKE_COST
 
 enum class Menu(val itemCategory: String, val itemName: String, val itemPrice: Int) {
-    APPETIZER_MUSHROOM_SOUP(Constants.APPETIZER, Constants.MUSHROOM_SOUP, Constants.MUSHROOM_SOUP_COST),
-    APPETIZER_TAPAS(Constants.APPETIZER, Constants.TAPAS, Constants.TAPAS_COST),
-    APPETIZER_CAESAR_SALAD(Constants.APPETIZER, Constants.CAESAR_SALAD, Constants.CAESAR_SALAD_COST),
+    APPETIZER_MUSHROOM_SOUP(APPETIZER, MUSHROOM_SOUP, MUSHROOM_SOUP_COST),
+    APPETIZER_TAPAS(APPETIZER, TAPAS, TAPAS_COST),
+    APPETIZER_CAESAR_SALAD(APPETIZER, CAESAR_SALAD, CAESAR_SALAD_COST),
 
-    MAIN_T_BONE_STEAK(Constants.MAIN, Constants.T_BORN, Constants.T_BORN_COST),
-    MAIN_BBQ_RIB(Constants.MAIN, Constants.BBQ, Constants.BBQ_COST),
-    MAIN_SEAFOOD_PASTA(Constants.MAIN, Constants.SEAFOOD_PASTA, Constants.SEAFOOD_PASTA_COST),
-    MAIN_CHRISTMAS_PASTA(Constants.MAIN, Constants.CHRISTMAS_PASTA, Constants.CHRISTMAS_PASTA_COST),
+    MAIN_T_BONE_STEAK(MAIN, T_BORN, T_BORN_COST),
+    MAIN_BBQ_RIB(MAIN, BBQ, BBQ_COST),
+    MAIN_SEAFOOD_PASTA(MAIN, SEAFOOD_PASTA, SEAFOOD_PASTA_COST),
+    MAIN_CHRISTMAS_PASTA(MAIN, CHRISTMAS_PASTA, CHRISTMAS_PASTA_COST),
 
-    DESSERT_CHOCOLATE_CAKE(Constants.DESSERT, Constants.CHOCO_CAKE, Constants.CHOCO_CAKE_COST),
-    DESSERT_ICE_CREAM(Constants.DESSERT, Constants.ICE_CREAM, Constants.ICE_CREAM_COST),
+    DESSERT_CHOCOLATE_CAKE(DESSERT, CHOCO_CAKE, CHOCO_CAKE_COST),
+    DESSERT_ICE_CREAM(DESSERT, ICE_CREAM, ICE_CREAM_COST),
 
-    DRINK_ZERO_COLA(Constants.DRINK, Constants.ZERO_COKE, Constants.ZERO_COKE_COST),
-    DRINK_RED_WINE(Constants.DRINK, Constants.RED_WINE, Constants.RED_WINE_COST),
-    DRINK_CHAMPAGNE(Constants.DRINK, Constants.CHAMPAGNE, Constants.CHAMPAGNE_COST)
+    DRINK_ZERO_COLA(DRINK, ZERO_COKE, ZERO_COKE_COST),
+    DRINK_RED_WINE(DRINK, RED_WINE, RED_WINE_COST),
+    DRINK_CHAMPAGNE(DRINK, CHAMPAGNE, CHAMPAGNE_COST)
 }
